b09e992e799a59988a3490a475532bf7e17d6b55
Damien LeBerrigaud <damien@initialcapacity.io>
2022-09-29 11:45:15 -0600

    Fix setting of ports when parseInt fails to parse.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,20 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
 
-const backendPort = parseInt(process.env.BACKEND_PORT) || 5001;
+const tryParseInt = (value: string | undefined): number | undefined => {
+    if (value === undefined) {
+        return undefined;
+    }
+
+    const parseResult = parseInt(value);
+    if (isNaN(parseResult)) {
+        return undefined;
+    }
+
+    return parseResult;
+};
+
+const backendPort = tryParseInt(process.env.BACKEND_PORT) ?? 5001;
 
 export default defineConfig({
     plugins: [
@@ -10,7 +23,7 @@ export default defineConfig({
         eslintPlugin({throwOnWarning: true, throwOnError: true})
     ],
     server: {
-        port: parseInt(process.env.PORT) || 3001,
+        port: tryParseInt(process.env.PORT) ?? 3001,
         proxy: {
             '/api': {
                 target: `http://127.0.0.1:${backendPort}`,
